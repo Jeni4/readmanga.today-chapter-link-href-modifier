00e40af70d047672ea79cd452c17f0a7e944f707
Jeni4 <Jeni4@users.noreply.github.com>
2016-09-14 14:37:10 +0200

    Added existing content to .user.js script

---
+++ readmanga-today-chapter-link-href-modifier.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @namespace    https://github.com/Jeni4/readmanga.today-chapter-link-href-modifier
+// @updateURL    
+// @downloadURL  
+//
+// @name         ReadManga.Today chapters container href modifier
+// @version      0.1
+// @description  
+//
+// @license      Creative Commons Attribution-Share Alike http://creativecommons.org/licenses/by-sa/3.0/
+// @copyright    
+//
+// @match        http://www.readmanga.today/
+// @grant        none
+// ==/UserScript==
+
+var mangaTitles = document.querySelectorAll( '.manga_updates a.manga_info' );
+var mangaChapters = document.querySelectorAll( '.manga_updates dl dd a' );
+
+for (var i=0; i<mangaTitles.length; i++) {
+	mangaTitles[i].href = mangaTitles[i].href + '#chapters_container';
+}
+
+for (var j=0; j<mangaChapters.length; j++) {
+	mangaChapters[j].href = mangaChapters[j].href + '/all-pages';
+}
